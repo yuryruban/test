d437c6998715bed45b0bbbe28d36d25e9a8e93e8
IURII RUBAN <rub.yury.o@gmail.com>
2022-10-17 13:59:41 +0300

    version 1.0(desktop)

---
+++ dist/js/script.js
@@ -0,0 +1,42 @@
+const selectHead = document.querySelector('.select__head');
+const selectList = document.querySelector('.select__list');
+const selectInput = document.querySelector('.select__input');
+const selectItems = document.querySelectorAll('.select__item');
+const rangeItem = document.querySelector('.forma__range-item');
+const formaFile = document.querySelector('.forma__file');
+const formaFileImg = document.querySelector('.forma__fileImg');
+
+
+selectHead.addEventListener('click', () => {
+	selectHead.classList.toggle('open');
+	selectList.classList.toggle('hidden');
+	selectList.classList.toggle('show');
+});
+
+selectItems.forEach(item => {
+	item.addEventListener('click', () => {
+		let text = item.textContent;
+		selectInput.value = text;
+		selectHead.textContent = text;
+		selectList.classList.toggle('hidden');
+		selectList.classList.toggle('show');
+		selectHead.classList.toggle('open');
+	});
+});
+
+
+rangeItem.addEventListener('mousemove', rangeChange);
+rangeItem.addEventListener('click', rangeChange);
+
+function rangeChange() {
+	document.getElementById('textInput').textContent = rangeItem.value;
+};
+
+
+formaFile.addEventListener('change', () => {
+	// document.querySelector('.forma__fileText').textContent = formaFile.value;
+	const fileNameArr = formaFile.value.split("\\");
+	const fileName = fileNameArr[fileNameArr.length - 1];
+	document.querySelector('.forma__fileText').textContent = fileName;
+	formaFileImg.src = 'icons/check.png';
+});
